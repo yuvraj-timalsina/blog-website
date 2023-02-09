082969f26239ba411e96b4812ba6369fc8acde90
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-02-09 18:48:46 +0545

    Margin on all pages removed

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -7,7 +7,7 @@
                     {{ __('Tags') }}
                 </h2>
             </div>
-            <div class="mt-4 flex md:mt-0 md:ml-4">
+            <div class="flex md:mt-0 md:ml-4">
                 <button type="button" onclick="window.location='{{ route('tags.create') }}'"
                         class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700">
                     Add Tag
